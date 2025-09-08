23f9b1509166a7e6c7424c283cf34aff8c47f957
DuoVZA <zayvlad2009@gmail.com>
2025-09-08 11:32:54 +0300

    hi

--- Task 7/main.js
+++ Task 7/main.js
@@ -1,4 +1,4 @@
-let GB = prompt("Enter sixe of SSD (in GB):", "123");
+let GB = prompt("Enter size of SSD (in GB):", "123");
 const MB = 820;
 let result = (GB * 1028) / MB;
-alert("There is a result" + result + " that can be in ur SSD");
+alert("There is a result " + parseInt(result) + " that can be in ur SSD");
